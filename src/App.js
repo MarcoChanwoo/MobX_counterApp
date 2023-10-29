7bf443be57988afc7b5a40c0913f5db5b187b563
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-29 21:48:46 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 
 function App(props) {
   const { myCounter } = props;
-  return <div className="App"></div>;
+  return (
+    <div style={{ textAlign: "center", padding: 16 }}>
+      카운트:
+      <br />
+      <br />
+      마이너스?:
+      <br />
+      <br />
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
 }
 
 export default App;
