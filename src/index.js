427a9a23de00efdeaa91e71b4c806fafb77d06dd
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-29 21:45:27 +0900

    updated

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const store = new counterStore();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App myCounter={store} />
   </React.StrictMode>
 );
 
